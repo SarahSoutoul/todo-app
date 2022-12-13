f7b3bb6eb8fc4e446791f277b320a21c3c83492a
Peritract <dan@getfutureproof.co.uk>
2022-12-13 17:39:45 +0000

    Refactor components

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,10 +1,28 @@
 import Todo from '../Todo';
 
 function TodoList({todos, setTodos}) {
+
+    const deleteTodo = (todo) => {
+        let filteredTodos = todos.filter(el => el !== todo)
+        setTodos(filteredTodos)
+    }
+
+    const completeTodo = (todo) => {
+        setTodos(todos.map((item) => {
+            if(item === todo) {
+                return {
+                    ...item,
+                    completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
+
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                {todos.map((todo) => <Todo todo={todo} setTodos={setTodos} todos={todos} />)}
+                {todos.map((todo, i) => <Todo todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} key={i} />)}
             </ul>
         </div>
     )
