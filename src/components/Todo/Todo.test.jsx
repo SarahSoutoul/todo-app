f4c7f236952fe154e902b6ffad713f81909ea71f
Peritract <dan@getfutureproof.co.uk>
2022-12-13 16:13:27 +0000

    Add clicking

--- src/components/Todo/Todo.test.jsx
+++ src/components/Todo/Todo.test.jsx
@@ -1,5 +1,6 @@
 import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import { screen, render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
@@ -18,7 +19,7 @@ describe("Todo component", () => {
 
     it("Is in the document", () => {
 
-        const elem = screen.getByRole("listitem")
+        const elem = screen.getByRole("listitem");
 
         expect(elem).toBeInTheDocument();
 
@@ -31,4 +32,17 @@ describe("Todo component", () => {
         expect(buttons.length).toEqual(2);
 
     });
+
+    it("Adds the completed class on click", async () => {
+
+        const compButton = screen.getByRole("button", { name: "Complete" });
+        const toDoItem = screen.getByRole("listitem").firstChild;
+
+        expect(toDoItem).not.toHaveClass("complete");
+
+        await userEvent.click(compButton);
+
+        expect(toDoItem).toHaveClass("complete");
+
+    })
 })
